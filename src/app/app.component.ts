53c56b9fd3b9639ccc5e01cec3faf02aa6c12157
JagerDAN <ag.darkhan@gmail.com>
2022-10-28 17:59:30 +0600

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NavigationStart, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  isMain: any;
+
+  constructor(private router: Router) {
+    router.events.pipe(filter(event => event instanceof NavigationStart))
+        .subscribe((event) => {
+          // console.log(event.url)
+        });
+  }
 }
