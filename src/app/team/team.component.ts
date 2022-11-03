960106474c33633e063fb802f7b096d84a265212
JagerDAN <ag.darkhan@gmail.com>
2022-11-03 18:35:50 +0600

    new commit 3

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -7,6 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
+  teams = [
+    [
+      {
+        title: 'продюссер',
+        team: [
+            'Есенбек Исламхан',
+            'Владимир Путин'
+        ]
+      },
+      {
+        title: 'Колорист',
+        team: [
+          'Дмитрий Медведев'
+        ]
+      },
+      {
+        title: 'Оператор',
+        team: [
+          'Дональд Трамп'
+        ]
+      },
+      {
+        title: 'Режиссер',
+        team: [
+          'Касым Жомарт Токаев'
+        ]
+      }
+    ],
+    [
+      {
+        title: 'Монтажер',
+        team: [
+          'Владимир Зеленский',
+          'Мишель Обама'
+        ]
+      },
+      {
+        title: 'Сценарист',
+        team: [
+          'Карменов Расул'
+        ]
+      },
+      {
+        title: 'Монтажер',
+        team: [
+          'Ангела Меркель'
+        ]
+      }
+    ]
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
